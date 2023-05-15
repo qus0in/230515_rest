e361d2c30c0ae52555660fcf0065d2f46a749691
qus0in <qus0in@gmail.com>
2023-05-15 15:54:10 +0900

    C. R.

--- src/main/java/io/playdata/rest/controller/StoreController.java
+++ src/main/java/io/playdata/rest/controller/StoreController.java
@@ -1,9 +1,55 @@
 package io.playdata.rest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.playdata.rest.model.StoreDTO;
+import io.playdata.rest.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.List;
+import java.util.Optional;
+
+@RestController // Controller <-> RestController
 @RequestMapping("/store")
 public class StoreController {
+    @Autowired
+    private StoreService storeService;
+
+    // C.R.U.D
+    // Create - Post
+//    @PostMapping("") // 생성이 되었을 때 생성된 데이터를 반환
+    @PostMapping // 생성이 되었을 때 생성된 데이터를 반환
+    // Body -> 요청을 받고, 반환
+    public StoreDTO createStore(@RequestBody StoreDTO store) {
+        return storeService.createStore(store);
+    }
+
+    // Read - Read (one) / Read All / search, filtering
+//    @GetMapping // ("")
+//    public List<StoreDTO> findAllStore() {
+//        return storeService.findAll();
+//    }
+
+    @GetMapping
+    public List<StoreDTO> findAllStoreSortByPrice(@RequestParam(required = false) Boolean price) {
+        // true은 오름차순으로, false은 내림차순으로
+        if (price == null) {
+            return storeService.findAll();
+        }
+        return storeService.findAll(price);
+    }
+
+    @GetMapping("/{id}") // id <- 경로에서 ID 받기
+    public Optional<StoreDTO> findById(@PathVariable Long id) {
+        return storeService.findById(id);
+    }
+
+    @GetMapping("/one") // query parameter로 ID 받기 // one?id=1
+    public Optional<StoreDTO> findByIdQuery(@RequestParam Long id) {
+        return storeService.findById(id);
+    }
+
+    @GetMapping("/search/{kwd}")
+    public List<StoreDTO> findByKwd(@PathVariable String kwd) {
+        return storeService.findByNameKwd(kwd);
+    }
 }
--- src/main/java/io/playdata/rest/service/StoreService.java
+++ src/main/java/io/playdata/rest/service/StoreService.java
@@ -1,12 +1,38 @@
 package io.playdata.rest.service;
 
+import io.playdata.rest.model.StoreDTO;
 import io.playdata.rest.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StoreService {
     @Autowired
     private StoreRepository storeRepository;
 
+    public StoreDTO createStore(StoreDTO store) {
+        return storeRepository.save(store);
+    }
+
+    public List<StoreDTO> findAll() {
+        return storeRepository.findAll();
+    }
+
+    public Optional<StoreDTO> findById(Long id) {
+        return storeRepository.findById(id);
+    }
+
+    public List<StoreDTO> findByNameKwd(String kwd) {
+        // findBy속성명 : 완전 일치
+        // findBy속성명Containing : 부분 일치 (포함)
+        return storeRepository.findByNameContaining(kwd); // 생성 규칙
+    }
+
+    public List<StoreDTO> findAll(boolean price) {
+        return price ? storeRepository.findAllByOrderByPriceAsc()
+                : storeRepository.findAllByOrderByPriceDesc();
+    }
 }
